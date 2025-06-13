c839c13eef1677c40b6390d2cadc9946d8c86e8a
jonathancalvin <jonathancalvin16@gmail.com>
2025-06-13 13:38:33 +0700

    Initial Commit

---
+++ FenScan/FenScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  FenScanApp.swift
+//  FenScan
+//
+//  Created by jonathan calvin sutrisna on 13/06/25.
+//
+
+import SwiftUI
+
+@main
+struct FenScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
