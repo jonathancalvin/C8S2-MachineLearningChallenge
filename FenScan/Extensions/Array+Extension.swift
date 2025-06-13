ce59c6069fa0d29436071aae171c998c478a04a4
kennethmyrwjy <145191763+kennethmyrwjy@users.noreply.github.com>
2025-06-13 16:19:08 +0700

    added notes comment
    comment stating function of each folder

--- FenScan/Extensions/Array+Extension.swift
+++ FenScan/Extensions/Array+Extension.swift
@@ -4,6 +4,24 @@
 //
 //  Created by jonathan calvin sutrisna on 13/06/25.
 //
+
+/* Example:
+ String Extension to add a function to capitalize the first letter of a string
+ 
+ extension String {
+     func capitalizeFirstLetter() -> String {
+         return prefix(1).capitalized + dropFirst()
+     }
+ }
+ Color.swift (Extensions)
+ extension Color {
+     static let primary = Color("PrimaryColor")
+     static let secondary = Color("SecondaryColor")
+ }
+
+
+ */
+
 extension Array where Element == String {
     //Add some func
 }
